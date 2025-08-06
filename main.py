7c61bd90c5e0bd18a33ff952966aa93a8b496e96
unknown <rafaelantoniocordero2002mail.com>
2025-08-06 19:25:14 -0400

    Puedo decir que informacion quiero que traiga

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from ast import Try
+from pydoc import text
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
@@ -21,9 +24,10 @@ prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system", 
-            """You are a research assistant that will help generate a research paper.
-            Answer the user query and use neccessary tools.
-            Wrap the output in this format and provide no other text\n{format_instructions}
+            """Eres un asistente de investigación que ayudará a generar un documento de investigación.
+            Responde siempre en el idioma del usuario.
+            Responde la consulta del usuario y usa las herramientas necesarias.
+            Envuelve la salida en este formato y no proporciones otro texto\n{format_instructions}
             """
         ),
         ("placeholder", "{chat_history}"),
@@ -33,10 +37,19 @@ prompt = ChatPromptTemplate.from_messages(
 ).partial(format_instructions=parser.get_format_instructions())
 
 
+agent = create_tool_calling_agent(
+    llm = llm, 
+    prompt=prompt,
+    tools=[]
+)
 
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=False)
+raw_response = agent_executor.invoke({"query": "What is the capital of France?"})
+""" print(raw_response) """
 
-
-response = llm.invoke("what is the capital of France?")
-
-print("Respuesta recibida:")
-print(response.content)
+try:
+    structured_response = parser.parse(raw_response.get("output"))
+    print(structured_response.sources)
+except Exception as e:
+    print(f"Error al procesar la respuesta: {e}", "Raw response", raw_response)
+    #print(raw_response.get("output")[0]["text"])
